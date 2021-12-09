dd7ffe970c9f36be973da6c4ad1ff7c9d77e6e43
Maycon Martins <maycon.rocha2100@gmail.com>
2021-12-08 21:28:11 -0300

    Finalização do CRUD

--- mains.js
+++ mains.js
@@ -1,29 +1,24 @@
-
-
-
-/* -----------------------CRUD-------------------- */
 const getLocalStorage = () => JSON.parse(localStorage.getItem('db_cliente')) ?? []
 const setLocalStorage = (db_cliente) => localStorage.setItem("db_cliente", JSON.stringify(db_cliente))
 
-/* ----Read------ */
+// ----Read------ 
  const readCLient = () => getLocalStorage() 
 
-/* ----Update------ */
-
- const updateClient = (index, client) => {
+// ----Update------ 
+const updateClient = (index, client) => {
     const db_cliente = readCLient()
     db_cliente[index] = client
     setLocalStorage(db_cliente)
 } 
 
-/* ----delete----- */
- const deleteClient = (index) => {
+// ----delete-----
+const deleteClient = (index) => {
     const db_cliente = readCLient()
     db_cliente.splice(index, 1)
     setLocalStorage(db_cliente)
 } 
 
-/* ----create------ */
+// ----create------ 
 const createCliente = (client) => {
     const db_cliente = getLocalStorage()
     db_cliente.push(client)
@@ -52,17 +47,24 @@ const saveCliente = () => {
             cell: document.getElementById('cell').value,
             cidade: document.getElementById('cidade').value
         }
+        const index = document.getElementById('nome').dataset.index
+            if (index==='new') {
         createCliente(client)
         clearFields()
         updateTable()
         fecharmodal()
+        } else {
+            updateClient(index, client)
+            updateTable()
+            fecharmodal()
+        }
     }
     
 }
 
-//mostrar os dados na tela do banco de dados
+// Mostrar os dados na tela, lendo do banco de dados.
 
-const createRow = (client) => {
+const createRow = (client, index) => {
     const newRow = document.createElement('tr')
     newRow.innerHTML = ` 
         <td>${client.nome}</td>
@@ -70,8 +72,8 @@ const createRow = (client) => {
         <td>${client.cell}</td>
         <td>${client.cidade}</td>
         <td>
-            <button type="button" class="button green">editar</button>
-            <button type="button" class="button red">excluir</button>
+            <button type="button" class="button green" id='edit-${index}'>editar</button>
+            <button type="button" class="button red" id='delete-${index}'>excluir</button>
          </td>`
 
     document.querySelector('#tableClient>tbody').appendChild(newRow)
@@ -88,31 +90,71 @@ const updateTable = () => {
     clearTable()
     db_client.forEach(createRow)
 } 
-/*  */
 
 updateTable()
 
-/* ------EVENTOS---------- */
 
-//modal
+// Action of edit and Delete
+
+const fillFilds = (client) => {
+    document.getElementById('nome').value = client.nome
+    document.getElementById('email').value = client.email
+    document.getElementById('cell').value = client.cell
+    document.getElementById('cidade').value = client.cidade
+    document.getElementById('nome').dataset.index = client.index
+
+}
+
+const editClient = (index) => {
+    const client = readCLient()[index]
+    client.index = index
+    fillFilds(client)
+    abrirModal()
+
+}
+
+const editDelete = (event) => {
+    if (event.target.type === 'button') {
+        const [action, index] = event.target.id.split('-')
+            if (action==='edit') {
+                editClient(index)
+            }
+            else {
+                const client = readCLient()[index]
+                const response = confirm(`Deseja realmente excluir o cliente ${client.nome}?`)
+                    if (response) {
+                        deleteClient(index)
+                        updateTable()
+                }
+            }
+    }
+}
+
+document.querySelector('#tableClient>tbody')
+    .addEventListener('click', editDelete)
+
+
+// ------EVENTOS---------- 
+
 const buttonAbrir = document.querySelector('#cadastrarCliente')
-    buttonAbrir.addEventListener('click', () => {
+    buttonAbrir.addEventListener('click',abrirModal)
+
+    function abrirModal(){
         const modal = document.querySelector('.modal')
         modal.classList.add('active')
-})
+    }
 
 const buttonFechar = document.querySelector("#modalClose")
     buttonFechar.addEventListener('click', fecharmodal)
 
+    function fecharmodal() {
+        clearFields()
+        const modal = document.querySelector('.modal')
+            modal.classList.remove('active')
+    }
+
 document.getElementById('cancelar').addEventListener('click', fecharmodal)
+document.getElementById('buttonCadastrar').addEventListener('click', saveCliente) 
 
-function fecharmodal() {
-    clearFields()
-    const modal = document.querySelector('.modal')
-        modal.classList.remove('active')
-}
 
-document.getElementById('buttonCadastrar').addEventListener('click', saveCliente) //Botão de cadastrar
-
-//--------------------------
 
